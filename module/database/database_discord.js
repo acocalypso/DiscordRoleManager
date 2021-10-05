5b02402d2bd2ef487b543d17a04de573905062ea
Aco <st.luzifer6663@htomail.com>
2021-10-05 10:32:12 +0200

    add timestamp to sqlconnection errors

--- module/database/database_discord.js
+++ module/database/database_discord.js
@@ -22,13 +22,13 @@ sqlConnectionDiscord = mysql.createPool({
 sqlConnectionDiscord.getConnection((err, connection) => {
 	if (err) {
 		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			console.error('Database connection was closed.')
+			console.error(helper.GetTimestamp + ' Database connection was closed.')
 		}
 		if (err.code === 'ER_CON_COUNT_ERROR') {
-			console.error('Database has too many connections.')
+			console.error(helper.GetTimestamp + ' Database has too many connections.')
 		}
 		if (err.code === 'ECONNREFUSED') {
-			console.error('Database connection was refused.')
+			console.error(helper.GetTimestamp + ' Database connection was refused.')
 		}
 	}
 
